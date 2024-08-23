ffbd8f96ec26b77243f0398f726ddeee9f6ce574
shugacodes <shugaglow@gmail.com>
2024-08-23 10:49:39 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuToggle = document.getElementById('mobile-menu');
+const navList = document.querySelector('.nav-list');
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    navList.classList.toggle('active');
+});
